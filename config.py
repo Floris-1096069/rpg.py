cdaa5644b4e84deb804b01eb5df73a68b3dd4be1
Floris-1096069 <1096069@hr.nl>
2026-09-24 18:50:33 +0200

    Feature, made controls more modular and robust.

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+import pygame
+
 class Config:
     def __init__(self):
         self.fps = 60
         self.resolution = (800, 600)
+        self.key_bindings = {
+            "rotate_left": [pygame.K_a, pygame.K_LEFT],
+            "rotate_right": [pygame.K_d, pygame.K_RIGHT],
+            "move_forward": [pygame.K_w, pygame.K_UP],
+            "move_backward": [pygame.K_s, pygame.K_DOWN],
+        }
--- src/graphics/renderer/camera.py
+++ src/graphics/renderer/camera.py
@@ -1,24 +1,34 @@
 import math
-import pygame
+
+from config import Config
 
 class Camera:
     def __init__(self, x=0, y=0, angle=0):
+        config = Config()
         self.x = x
         self.y = y
         self.angle = angle
-        self.speed = 5
-        self.rotation_speed = 0.05
+        self.speed = 5  
+        self.rotation_speed = 0.07
+        self.keybindings = config.key_bindings
         
     def update(self, input_state):
         keys = input_state["keys"]
-        if pygame.K_a in keys or pygame.K_LEFT in keys:
-            self.angle -= self.rotation_speed
-        if pygame.K_d in keys or pygame.K_RIGHT in keys:
-            self.angle += self.rotation_speed
 
-        if pygame.K_w in keys or pygame.K_UP in keys:
+        if self._is_action_pressed("rotate_left", keys):
+            self.angle -= self.rotation_speed
+        if self._is_action_pressed("rotate_right", keys):
+            self.angle += self.rotation_speed
+    
+        if self._is_action_pressed("move_forward", keys):
             self.x += self.speed * math.cos(self.angle)
             self.y += self.speed * math.sin(self.angle)
-        if pygame.K_s in keys or pygame.K_DOWN in keys:
+        if self._is_action_pressed("move_backward", keys):
             self.x -= self.speed * math.cos(self.angle)
             self.y -= self.speed * math.sin(self.angle)
+
+    def _is_action_pressed(self, action, keys):
+        for key in self.keybindings.get(action, []):
+            if key in keys:
+                return True
+        return False
